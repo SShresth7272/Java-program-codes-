f18eb1ac7325833d661578d3d66cfb7e7146b6f5
shubh <shubh@LAPTOP-4GF1DSMQ>
2025-05-17 09:10:17 +0530

    First Commit

---
+++ SumOfTwoNumbers/src/SumOfTwoNumbers/SumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package SumOfTwoNumbers;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+	    
+	     System.out.print("Input the first number: ");
+	    int num1 = input.nextInt();
+	     System.out.print("Input the second number: ");
+	    int num2 = input.nextInt();
+	    int sum = num1 + num2;
+	    System.out.println();
+	    System.out.println("Sum: " + sum);
+	}
+
+}
