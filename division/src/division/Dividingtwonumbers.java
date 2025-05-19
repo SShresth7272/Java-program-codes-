3f55d455e2d5e0cc6efaf1f97a98827dccd478ef
shubh <shubh@LAPTOP-4GF1DSMQ>
2025-05-19 07:06:31 +0530

    Commit

---
+++ division/src/division/Dividingtwonumbers.java
@@ -0,0 +1,20 @@
+//package division;
+
+//public class Dividingtwonumbers {
+	
+	//int divide = 50/3;
+	
+	//System.out.println("divide");
+//
+}//
+
+
+public class division {
+    public static void main(String[] args) {
+        // Calculate the result of the division 50/3
+        int result = 50 / 3;
+
+        // Print the result of the division
+        System.out.println(result);
+    }
+} 
