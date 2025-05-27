d206e131f59c0a8cfc1ad5ea049d22ad715bc49c
shubh <shubh@LAPTOP-4GF1DSMQ>
2025-05-27 09:00:07 +0530

    Commit

---
+++ MethodDemo/src/userdefinedmethod/ExternalClass.java
@@ -0,0 +1,12 @@
+package userdefinedmethod;
+
+public class ExternalClass extends userdefinedmethoddemo {
+	
+	public static void main(String[] args) {
+		
+		userdefinedmethoddemo.helloworld();
+		
+		sayHI("Shubham");
+	}
+
+}
---
+++ MethodDemo/src/userdefinedmethod/userdefinedmethoddemo.java
@@ -0,0 +1,90 @@
+/*//package userdefinedmethod;
+
+public class userdefinedmethoddemo {
+	 
+	public void helloworld() {
+		System.out.println("Hello World");
+	}
+     public static void main(String[] args) {
+		
+    	 userdefinedmethoddemo obj= new userdefinedmethoddemo();
+    	 obj.helloworld(); 
+			
+		}
+		
+	}*/
+
+
+/*package userdefinedmethod;
+
+public class userdefinedmethoddemo {
+	 
+	static public void helloworld() {
+		System.out.println("Hello World");
+	}
+     public static void main(String[] args) {
+		
+    	 helloworld(); 
+			
+		}
+		
+	}*/
+
+
+/*
+ * package userdefinedmethod;
+ * 
+ * public class userdefinedmethoddemo {
+ * 
+ * int a=5,result;
+ * 
+ * static public void helloworld() { System.out.println("Hello World"); }
+ * 
+ * public int square() { result= a*a; return result; } public static void
+ * main(String[] args) {
+ * 
+ * helloworld(); userdefinedmethoddemo obj= new userdefinedmethoddemo(); int x=
+ * obj.square(); System.out.println(x);
+ * 
+ * }
+ * 
+ * }
+ */
+
+
+/*
+ * package userdefinedmethod;
+ * 
+ * public class userdefinedmethoddemo {
+ * 
+ * int a=5,result;
+ * 
+ * static public void helloworld() { System.out.println("Hello World"); }
+ * 
+ * public int square() { result= a*a; return result; }
+ * 
+ * 
+ * public void sayHi(String name) { System.out.println("Hi "+name); } public
+ * static void main(String[] args) {
+ * 
+ * // helloworld(); userdefinedmethoddemo obj= new userdefinedmethoddemo();
+ * //int x= obj.square(); // System.out.println(x); obj.sayHi("Shubham");
+ * 
+ * }
+ * 
+ * }
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
